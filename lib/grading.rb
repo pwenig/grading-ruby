87c2aca1138438e3849579222e023575b566b549
Paul Wenig <pwenig@gmail.com>
2014-06-18 10:13:00 -0600

    Analyed entire array

--- lib/grading.rb
+++ lib/grading.rb
@@ -7,10 +7,20 @@ class Grading
   def analyze
     trend = []
     i=0
+    while i <= @grades.length-2
     if @grades[i] > @grades[i+1]
       trend << :down
-    else
+      i+=1
+    elsif
+    @grades[i] < @grades[i+1]
       trend << :up
+      i+=1
+    elsif
+    @grades[i] == @grades[i+1]
+      trend << :even
+      i+=1
     end
+    end
+    trend
   end
 end
--- spec/grading_spec.rb
+++ spec/grading_spec.rb
@@ -10,12 +10,21 @@ describe Grading do
     expect(actual).to eq expected
   end
 
-  # it "compares the first three items in an array" do
-  #   grades = [6,3,5]
-  #   grade_trend = Grading.new(grades)
-  #   actual = grade_trend.analyze
-  #   expected = [:down, :up]
-  #   expect(actual).to eq expected
-  # end
+  it "compares the first three items in an array" do
+    grades = [6,3,5]
+    grade_trend = Grading.new(grades)
+    actual = grade_trend.analyze
+    expected = [:down, :up]
+    expect(actual).to eq expected
+  end
+
+  it "compares the entire array of grades" do
+    grades = [6,3,5,4,3,4,4,5]
+    grade_trend = Grading.new(grades)
+    actual = grade_trend.analyze
+    expected = [:down,:up,:down,:down,:up,:even,:up]
+    expect(actual).to eq expected
+
+  end
 
 end
